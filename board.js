f3a6a5368f4e3a2ba4049400f7df5bb4d660bb9d
kevany23 <kevan23yang@gmail.com>
2019-04-08 20:30:27 -0700

    Now shows pieces

--- board.js
+++ board.js
@@ -1,6 +1,23 @@
+/*
+Class for board object/data functionality
+Testing separation of concerns to the max
+*/
 class Board {
   constructor(layout) {
     this.layout = layout;
+  }
+  handleClick(x, y) {
+    if (this.checkValid(x, y) == -1) {
+      return;
+    }
+    this.addPiece(x, y, "b");
+  }
+  checkValid(x, y) {
+    if (this.layout[x][y] != "x") {
+      console.log("Invalid move");
+      return -1;
+    }
+    console.log("Valid move");
   }
   getLayout() {
     return this.layout;
@@ -8,4 +25,12 @@ class Board {
   addPiece(x, y, color) {
     this.layout[x][y] = color;
   }
+}
+
+/*
+For pieces
+*/
+
+class Piece {
+
 }
--- game.js
+++ game.js
@@ -8,6 +8,7 @@ Global constants here
 */
 const WIDTH = 640;
 const HEIGHT = 640;
+const PIECE_SIZE = 30;
 
 var gameBoard;
 
@@ -16,22 +17,7 @@ Initially loads board
 */
 function loadGame() {
   var canvas = document.getElementById("game");
-  var ctx = canvas.getContext('2d');
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.strokeStyle = 'black';
-  for (let i = (WIDTH/8); i < WIDTH; i+= (WIDTH/8)) {
-    ctx.strokeStyle = 'black';
-    ctx.beginPath();
-    ctx.moveTo(i, 0);
-    ctx.lineTo(i, HEIGHT);
-    ctx.stroke();
-  }
-  for (let j = (HEIGHT/8); j < HEIGHT; j+= (HEIGHT/8)) {
-    ctx.beginPath();
-    ctx.moveTo(0, j);
-    ctx.lineTo(WIDTH, j);
-    ctx.stroke();
-  }
+  drawBoard();
   // enables clicking on canvas
   canvas.addEventListener('click', onCanvasClick);
   var a = [];
@@ -42,9 +28,13 @@ function loadGame() {
     }
     a.push(b);
   }
-  console.log(a);
+  a[3][3] = "b";
+  a[4][4] = "b";
+  a[3][4] = "w";
+  a[4][3] = "w";
   gameBoard = new Board(a);
   console.log(gameBoard.getLayout());
+  drawPieces();
 }
 
 /*
@@ -53,12 +43,16 @@ storing data on pieces
 */
 
 function onCanvasClick(event) {
-  console.log("Clicked");
   //console.log(event);
   let x = event.offsetX;
   let y = event.offsetY;
   var pos = getPosition(x, y);
   console.log("x: " + pos.x + "\ty: " + pos.y);
+  var valid = gameBoard.handleClick(pos.x, pos.y);
+  if (valid == -1) {
+    return;
+  }
+  updateBoard();
 }
 
 function getPosition(x, y) {
@@ -66,4 +60,59 @@ function getPosition(x, y) {
   pos.x = Math.floor(x / (WIDTH/8));
   pos.y = Math.floor(y / (HEIGHT/8));
   return pos;
+}
+
+function updateBoard() {
+  drawBoard();
+  drawPieces();
+}
+
+function drawBoard() {
+  var canvas = document.getElementById("game");
+  var ctx = canvas.getContext('2d');
+  ctx.rect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "green";
+  ctx.fill();
+  ctx.strokeStyle = 'black';
+  for (let i = 0; i <= WIDTH; i+= (WIDTH/8)) {
+    ctx.strokeStyle = 'black';
+    ctx.beginPath();
+    ctx.moveTo(i, 0);
+    ctx.lineTo(i, HEIGHT);
+    ctx.stroke();
+  }
+  for (let j = 0; j <= HEIGHT; j+= (HEIGHT/8)) {
+    ctx.strokeStyle = 'black';
+    ctx.beginPath();
+    ctx.moveTo(0, j);
+    ctx.lineTo(WIDTH, j);
+    ctx.stroke();
+  }
+}
+
+function drawPieces() {
+  var canvas = document.getElementById("game");
+  var ctx = canvas.getContext('2d');
+  ctx.strokeStyle = 'black';
+  let pieceList = gameBoard.getLayout();
+  for (let i = 0; i < pieceList.length; i++) {
+    for (let j = 0; j < pieceList[i].length; j++) {
+      if (pieceList[i][j] == "w") {
+          ctx.strokeStyle = 'white';
+        ctx.beginPath();
+        ctx.arc(i*(WIDTH/8)+(WIDTH/16), j*(HEIGHT/8)+(HEIGHT/16), PIECE_SIZE, 0, 2*Math.PI);
+        ctx.stroke();
+        ctx.fillStyle = "white";
+        ctx.fill();
+      }
+      else if (pieceList[i][j] == "b") {
+        ctx.strokeStyle = 'black';
+        ctx.beginPath();
+        ctx.arc(i*(WIDTH/8)+(WIDTH/16), j*(HEIGHT/8)+(HEIGHT/16), PIECE_SIZE, 0, 2*Math.PI);
+        ctx.stroke();
+        ctx.fillStyle = "black";
+        ctx.fill();
+      }
+    }
+  }
 }
